bb93211f2f00b8184affe5e9456f1d50a302b5cf
lgift <lpgift@my.waketech.edu>
2024-03-06 13:53:58 -0500

    enmies
    enmies

---
+++ enemies.py
@@ -0,0 +1,13 @@
+<<<<<<< Updated upstream
+=======
+# common enemies
+enemies = {"Bear": {"health": 20, "damage": 5},
+           "Wolf": {"health": 20, "damage": 5},
+           "Troll": {"health": 20, "damage": 5},
+           "Goblin": {"health": 20, "damage": 5},
+           "Psychotic Little Fairy": {"health": 20, "damage": 5}
+           }
+
+elite_enemies = {}
+ 
+>>>>>>> Stashed changes
