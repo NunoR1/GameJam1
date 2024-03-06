6e618bffc9459dc8b42849e9e5b9b0cc034c5748
Nuno <61714135+NunoR1@users.noreply.github.com>
2024-03-06 12:59:04 -0500

    small changes

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from time import sleep
 
 inventory = []
 
-player = [10, 4, 5]
-
 
 def main():
     game_start()
@@ -11,16 +9,23 @@ def main():
 
 def game_start():
 
-    print("You are in a path in the woods.\n")
+    print("You are in a path in the woods.")
     sleep(1)
-    print("The darkness of the forest looms around you.\n")
+    print("The darkness of the forest looms around you.")
     sleep(1)
-    print("You are on a quest to slay a dragon\n")
+    print("You are on a quest to slay a dragon")
     sleep(1)
-    print("The end of the path is nearing\n")
+    print("The end of the path is nearing")
     sleep(1)
-    print("You stumble upon an old man.\n")
-    player_name = input("\"What is your name young traveler?\"\n")
+    print("You stumble upon an old man.")
+    player_name = input('"What is your name young traveler?"\n--')
+    print(f"{player_name}")
+    sleep(1)
+    print('"That is a wonderful name."')
 
+
+
+def inventory():
+    pass
 
 main()
