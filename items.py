fd093f1edb476b1a76c74fd81759808d0177bd9c
lgift <lpgift@my.waketech.edu>
2024-03-06 13:58:04 -0500

    item
    teism

---
+++ items.py
@@ -0,0 +1,8 @@
+# epic items for epic stuff
+
+weapons = ["Stick w/ Rock", "Rusty Knife", "Sturdy Spear", "Balanced Sword", "The Sword"]
+grimoire = ["Blast", "Recovery", "Spirit"]
+armors = ["Rusty Armor", "Light Armor", "Heavy Armor"]
+shields = ["Wooden Shield", "Rusty Metal Shield", "Sturdy Shield"]
+potions = ["Small Health Potion", "Large Health Potion"]
+consumables = [""]
