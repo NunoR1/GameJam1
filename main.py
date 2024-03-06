d126fc6ade84f6b37bfffc62ea21f7dc5907c462
Nuno <61714135+NunoR1@users.noreply.github.com>
2024-03-06 12:30:33 -0500

    starter code

---
+++ main.py
@@ -0,0 +1,26 @@
+from time import sleep
+
+inventory = []
+
+player = [10, 4, 5]
+
+
+def main():
+    game_start()
+
+
+def game_start():
+
+    print("You are in a path in the woods.\n")
+    sleep(1)
+    print("The darkness of the forest looms around you.\n")
+    sleep(1)
+    print("You are on a quest to slay a dragon\n")
+    sleep(1)
+    print("The end of the path is nearing\n")
+    sleep(1)
+    print("You stumble upon an old man.\n")
+    player_name = input("\"What is your name young traveler?\"\n")
+
+
+main()
