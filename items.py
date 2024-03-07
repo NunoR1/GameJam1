6977e8fdb377aabec3aaa5d02ef95d9cdb57d0f9
lgift <lpgift@my.waketech.edu>
2024-03-07 13:56:12 -0500

    shop stuff
    shop function it displays stuff very cool

--- items.py
+++ items.py
@@ -1,8 +1,43 @@
-# epic items for epic stuff
+# Weapons
+weapon_oldsword = {"name":"Old Sword", "attack":3, "gold_value":5, "type":"weapon", "magic_mod":1}
+weapon_goodsword = {"name":"Sword", "attack":6, "gold_value":12, "type":"weapon", "magic_mod":1}
+weapon_staff = {"name":"Staff", "attack":3, "gold_value":30, "type":"weapon", "magic_mod":2.5}
+weapon_axe = {"name":"Axe", "attack":8, "gold_value":18, "type":"weapon", "magic_mod":0.8}
+weapon_spear = {"name":"Spear", "attack":7, "gold_value":15, "type":"weapon", "magic_mod":1}
+weapon_greatsword = {"name":"Greatsword", "attack":10, "gold_value":30, "type":"weapon", "magic_mod":0.5}
+weapon_magicsword = {"name":"Magic Sword", "attack":7, "gold_value":30, "type":"weapon", "magic_mod":1.75}
 
-weapons = ["Stick w/ Rock", "Rusty Knife", "Sturdy Spear", "Balanced Sword", "The Sword"]
-grimoire = ["Blast", "Recovery", "Spirit"]
-armors = ["Rusty Armor", "Light Armor", "Heavy Armor"]
-shields = ["Wooden Shield", "Rusty Metal Shield", "Sturdy Shield"]
-potions = ["Small Health Potion", "Large Health Potion"]
-consumables = [""]
+weapons = {"Old Sword":weapon_oldsword, "Sword":weapon_goodsword, "Staff":weapon_staff, "Axe":weapon_axe, "Spear":weapon_spear, "Greatsword":weapon_greatsword, "Magic Sword":weapon_magicsword}
+
+# Armor
+armor_cloth = {"name":"Cloth Armor", "hp_buff": 5, "gold_value":5, "type":"armor"}
+armor_leather = {"name":"Leather Armor", "hp_buff": 10, "gold_value":15, "type":"armor"}
+armor_tabard = {"name":"Tabard", "hp_buff": 20, "gold_value":25, "type":"armor"}
+armor_mail = {"name":"Mail Armor", "hp_buff": 35, "gold_value":40, "type":"armor"}
+
+armors = {"Cloth Armor":armor_cloth, "Leather Armor":armor_leather, "Tabard":armor_tabard, "Mail Armor":armor_mail}
+
+# Shields
+shield_plank = {"name":"Plank Shield", "hp_buff": 5, "gold_value":7, "type":"shield"}
+shield_round = {"name":"Round Shield", "hp_buff": 10, "gold_value":15, "type":"shield"}
+shield_tower = {"name":"Tower Shield", "hp_buff": 20, "gold_value":25, "type":"shield"}
+
+shields = {"Plank Shield":shield_plank, "Round Shield":shield_round, "Tower Shield":shield_tower}
+
+# Spellbooks
+spell_book = {"name":"Spellbook", "offense_spell_name":"Blast", "offense_spell_damage":6, "offense_spell_cost":3, "defense_spell_name":"Heal", "defense_spell_healing":4, "defense_spell_cost": 3, "gold_value":30, "type":"spellbook"}
+
+books = {"Spellbook":spell_book}
+
+offhand = {"Plank Shield":shield_plank, "Round Shield":shield_round, "Tower Shield":shield_tower, "Spellbook":spell_book}
+
+# Consumables
+
+item_healpotion = {"name":"Healing Potion", "effect":"health_restore", "strength":10, "is_magic": False, "gold_value":8, "type":"consumable"}
+item_manapotion = {"name":"Mana Potion", "effect":"mana_restore", "strength":10, "is_magic": False, "gold_value":8, "type":"consumable"}
+item_lightningWand = {"name":"Wand of Lightnging", "effect":"damage", "strength":10, "is_magic": True, "gold_value":16, "type":"consumable"}
+item_crossbow = {"name":"Crossbow", "effect":"damage", "strength":12, "is_magic": False, "gold_value":14, "type":"consumable"}
+
+consumables = {"Healing Potion":item_healpotion, "Mana Potion":item_manapotion, "Lightning Wand":item_lightningWand, "Crossbow":item_crossbow}
+
+item_pool = [weapon_axe, weapon_goodsword, weapon_greatsword, weapon_magicsword, weapon_oldsword, weapon_spear, weapon_staff, armor_cloth, armor_leather, armor_mail, armor_tabard, shield_plank, shield_round, shield_tower, spell_book, item_crossbow, item_healpotion, item_lightningWand, item_manapotion]
--- main.py
+++ main.py
@@ -1,26 +1,214 @@
 from time import sleep
+import random
+import items
 
-inventory = []
-
-player = [10, 4, 5]
+player_stats = {"atk": 0, "max_hp": 0, "cur_hp": 0, "max_mana": 0, "cur_mana": 0, "gold": 0}
+inventory = {"main_hand": [],
+             "off_hand": [],
+             "armor": [],
+             "consumables": []}
 
 
 def main():
-    game_start()
+    while player_stats["cur_hp"] > 0:
+        pass
+    game_over()  # show game over
 
 
 def game_start():
+    # Opening dialogue
+    print("You are in a path in the woods.")
+    sleep(1)
+    print("The darkness of the forest looms around you.")
+    sleep(1)
+    print("You are on a quest to slay a dragon")
+    sleep(1)
+    print("The end of the path is nearing")
+    sleep(1)
+    print("You stumble upon an old man.")
+    player_name = input('"What is your name young traveler?"\n--')
+    print(f'"{player_name}"')
+    sleep(1)
+    print('"That is a wonderful name."')
+    print(f'"This is a treacherous path, {player_name}"')
+    return player_name
 
-    print("You are in a path in the woods.\n")
-    sleep(1)
-    print("The darkness of the forest looms around you.\n")
-    sleep(1)
-    print("You are on a quest to slay a dragon\n")
-    sleep(1)
-    print("The end of the path is nearing\n")
-    sleep(1)
-    print("You stumble upon an old man.\n")
-    player_name = input("\"What is your name young traveler?\"\n")
+
+def show_inventory():
+    print(f"""Main Hand: {inventory['main_hand']}
+    Off Hand: {inventory['off_hand']}
+    Armor: {inventory['armor']}
+    Consumables: {inventory['consumables']}
+    Money: {player_stats['gold']}""")
 
 
-main()
+def merchant():
+    for let in "\nWelcome tired traveler...":
+        print(let, end='')
+        sleep(0.05)
+    sleep(1)
+    for let in "\nTake a look around and see if you fancy an item...":
+        print(let, end='')
+        sleep(0.05)
+    sleep(1)
+
+    print("\n\nSHOP ITEMS")
+    print("----------")
+
+    current_items = []
+    random_item = random.choice(list(items.item_pool))
+    current_items.append(random_item)
+    random_item = random.choice(list(items.item_pool))
+    current_items.append(random_item)
+    random_item = random.choice(list(items.item_pool))
+    current_items.append(random_item)
+    random_item = random.choice(list(items.item_pool))
+    current_items.append(random_item)
+    random_item = random.choice(list(items.item_pool))
+    current_items.append(random_item)
+    random_item = random.choice(list(items.item_pool))
+    current_items.append(random_item)
+
+    item_num = 0
+    for item in current_items:
+        item_num += 1
+        print(f"{item_num}.", item["name"], "-", item["gold_value"], "Gold")
+
+    print("----------")
+
+    purchase = input("Which # item would you like to buy? (or e to exit) \n--")
+
+    if purchase.lower() == 'e':
+        item_purchased = False
+
+    # if item_purchased:
+    #     for let in "\nEnjoy your purchase traveler... wishing that your item serves you well.":
+    #         print(let, end='')
+    #         sleep(0.05)
+    #     sleep(1)
+    # elif not item_purchased:
+    #     for let in "\nCome back anytime.":
+    #         print(let, end='')
+    #         sleep(0.05)
+    #
+    #     sleep(1)
+
+
+def fight(enemy):
+    print(f"You encounter {enemy}")
+    turn_counter = 0
+    while enemy["hp"] > 0 and player_stats["atk"] > 0:
+        if 0 % 2 == 0:  # Players turn
+            player_choice = input(f"What will {player_name} \nAttack \nMagic \nItems \nDefend \n--").lower()
+            if player_choice == "attack":
+                print(f"{player_name} uses his weapon")
+                print(f"{enemy} loses {player_stats['atk']} health")
+            elif player_choice == "magic":
+                if spellbook in inventory["off_hand"]:
+                    pass
+            elif player_choice == "items":
+                show_inventory()
+                print(f"What will {player_name} use?")
+                item_choice = input("--").lower()
+                if item in inventory["consumables"]:
+
+                    continue
+                else:
+                    print(f"You don't have {item_choice}")
+                    continue
+            elif player_choice == "defend":
+                enemy[atk] = enemy[atk] * .80
+            else:
+                print("Not possible")
+        else:  # Enemy turn
+            print(f"{enemy} attacks")
+            hurt = enemy["damage"] * random.randint(0,
+                                                    3)  # multiplies the damage value the enemy deals by a random value 0-3 so that it can miss, do a light, medium, or heavy
+            if hurt != 0:
+                print(f"{enemy} dealt {hurt} damage")
+                player_stats["cur_hp"] - hurt
+            else:
+                print(f"{enemy} missed")
+
+    turn_counter += 1
+
+
+def game_over():
+    print("""                                                                                                    
+                                        %%###########                                               
+                                     %%#########%%###%                                              
+                                  %%%%%%%%%%%%#########%                                            
+                               %%%%%%%%%#*++*#####%%%###%                                           
+                             %%%%%%%%%%#*+++==+++#%%%%%%%%                                    ++    
+                            %%%%%%%%%%%*+**+=====+#%%%%%%%                         %   +===+++====+ 
+                            %%%%%#%%%%%%*+++=====+#%%%%%%%%%%###%%%###%####%%%#########=-----=+***+ 
+                                ##%%%%@%%*+++===*#%%%%%%%%%%###%#%%%%%%####%#%%%%######=----=+++    
+                                   %%%@@@%#*++*%%%%%%%%%%%%%###%%%##%%#########%######              
+                                     @@@@@@@@@@@%%%%%%%%%%%%%###%######%######%                     
+     **++                             %%%%%%%%%%%%%%%%%%%%%%%%##########%%                          
+    *+***++                        %%%%%%%%%%%%%%%%%%%%%%%%%%%%##                                   
+    +++**+==++++               %%%%%%%%%%%%%@%%%%%%%%%%%%%%%%%%###                                  
+    ==+++========+++++++   %%%%%%%%%%%%%%%%%@%%%%%%%%%%%%%%%%%%###                                  
+           ++============+*%%%%%%%%%%%%%%@%@%%%%%%%%%%%%%%%%%%####                                  
+               ++===========*%%%%%%%%%%%%@%%@@%%%%%%%%%%%%%%%%####                                  
+                    ++======*%%%%%%%%      %@@%%%%%%%%%%%%%%%#####                                  
+                                             @%%%%%%%%%%%%%%%%####                                  
+                                              %@%%%%%%%%%%%%%%%###                                  
+                                              @@%%%%%%%###########                                  
+                                              @@@%%%%%#####*+===**                                  
+                                              @@@%%@@@@@%%%%%%%#+#                                  
+                                  %%%%%%%%@@@@@@@%%%%%%%%%%%%%##%#                                  
+                      %%%%%%%%%%%%%%%%%%%%%%@@@%@@@@@%%%%%%#%%####                                  
+                    %%%%%%%%%%%%%%%%%%%%%%@@@@%%@@%%%%%%%#%%######                                  
+                    %%%%%%%%%%%%%%%%%%%%%%@@@@@@@@%%%%%%%%%%%#####                                  
+                    %%%%%%%%%%%%%%%%%%%%%@@@@@@@@%%%%%%%%%%%%%####                                  
+                    %%%%%%%%%%%%%%%%%%%%%@@@@@@@%%%%%%%%#########                                   
+                      %%%%%%%%%%%@@@%%%%@@@@@@@%%##%#############                                   
+                       %%%%%%%%%%%@        @%%%%%#######%%#####%                                    
+                        %%%%%%%%%%@@         %%%#######%%#%###                                      
+                          %%%%%%%%%@@       %%%%######%%####                                        
+                           %%%%%%%@@@     @%%%######%####                                           
+                            %%%%%%@@@@@   %%%%%####%####                                            
+                             %%%%%%@@@@@ %%%%%#%%%#####                                             
+                              %%%%%@@@%%%%####%%%#####                                              
+                               %%%%%%%######%%%%%###                                                
+                                %%%##%%%%%%%%%%###                                                  
+                                 ##%%%%##%%%%%%%#                                                   
+                                 **#*#%%%%%%%%%%#%                                                  
+                                 +*#+*#%%%%%%%%%###                                                 
+                                 +**++*#%%%%%%%%%####                                               
+                                  #%%#*#%%%%%%%%%%%###                                              
+                                  %%#*####%%%%%%%%%%###                                             
+                                  ##*####% %%%%%%#######                                            
+                                 %#*####     %%%#########                                           
+                                  *####        %##########                                          
+                                                %##########                                         
+                                                 %%#########                                        
+                                                   %#########% ##*#                                 
+                                                    %########*##*=+                                 
+                                                      #######*##*--+                                
+                                                       ####***##+:--                                
+                                                        *****###+:::                                
+                                                       #*+*##*+--:::                                
+                                                        ===++-++-::-                                
+                                                          ++*=-++-:-                                
+                                                           +**+=++--                                
+                                                            *#####--                                
+                                                            ######-=                                
+                                                             %%###-+                                
+                                                             %%##+-                                 
+                                                             %###=+                                 
+                                                              %#++                                  
+                                                                                                    """)
+    sleep(2)
+    print("You have met a terrible fate.")
+    restart = input("Restart?\nYes         No\n--").lower()
+    if restart == "yes":
+        main()
+    else:
+        pass
+
+
+player_name = game_start()
+show_inventory()
+merchant()
