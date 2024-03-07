4d1be20c238ba523bde745f4fecf7eb697bf212a
pfthomas-1 <145382532+pfthomas-1@users.noreply.github.com>
2024-03-07 13:13:41 -0500

    Updates Paths
    finished a few rooms, deleted a few others

--- paths.py
+++ paths.py
@@ -1,29 +1,28 @@
 # Rooms
+import main
+import enemies
+import items
 import random
 
-room_types = ["monster", "monster_elite", "merchant", "sacrafice", "floor_item", "trial", "boss", "campfire", "loot"]
+room_types = ["monster", "merchant", "floor_item", "trial", "boss", "campfire",]
 
 def monster_room():
-    pass
     # encounter a random enemy, win for a reward
-
-def monster_elite_room():
-    pass
-    # encounter a random elite enemy, win for a greater reward
+    monster = random.choice(enemies.enemies.keys())
+    print(f"A {monster["name"]}, stands before you. It must be defeated if you wish to complete your quest")
+    main.fight(monster)
+    print(f"The{monster["name"]} has been defeated, you gained {monster["gold"]} gold!")
+    main.playerstats["gold"] += monster["gold"]
 
 def merchant_room():
     pass
     # encounter a merchant that sells random items
 
-def sacrafice_room():
-    pass
-    # sacrafice an item, gain a permanent buff depending on the item's gold cost
-
 def floor_item_room():
     pass
     # choose between three items until you have 3 consumables
 
-def trial_room(player_gold, player_attack, player_health):
+def trial_room():
     # if the player completes a trial, gain a buff
     trial_list = ["wealth", "vitality"]
     trial_type = random.choice(trial_list)
@@ -32,10 +31,10 @@ def trial_room(player_gold, player_attack, player_health):
     if trial_type == "wealth":
         print("The ghost of a long-dead merchant floats before you \"Traveler,\" he says, \"I offer the trial of wealth, carry 55 gold and recieve my boon.\"")
         input("Press any key to commence the trial of wealth")
-        if player_gold >= 55:
+        if main.player_stats["gold"] >= 55:
             print("The merchant nods, \"You have passed my test, you may receive my boon. The blessing of life\"")
             print("MAX HP INCREASED")
-            player_hp_bonus += 15
+            main.player_stats["max_hp"] += 15
         else:
             print("The merchant frowns, \"You do not have enough gold, you cannot receive my boon.\"")
     
@@ -43,10 +42,10 @@ def trial_room(player_gold, player_attack, player_health):
     if trial_type == "vitality":
         print("The ghost of a slain knight floats before you \"Traveler,\" he says, \"I offer the trial of vitality, possess 50 max health and recieve my boon.\"")
         input("Press any key to commence the trial of vitality")
-        if player_health >= 50:
+        if main.player_stats["max_hp"] >= 50:
             print("\"You have passed my test, traveler.\" The knight bows his head, \"With the boon of strength, your blow shall strike true. May you succeed where I have failed, slay that monster.\"")
             print("DAMAGE INCREASED")
-            player_attack_bonus += 5
+            main.player_stats["atk_up"] += 5
         else:
             print("\"I apologize,\" the knight says, \"you lack the endurance to receive my blessing.\"")
 
@@ -55,13 +54,11 @@ def boss_room():
     # encounter the biome's boss.
 
 def campfire_room():
-    pass
     # restore health and mana
-    # next room after a boss is always a campfire
-
-def loot_room():
-    pass
-    # gain three random items, can be main_hand, off_hand, armor, or consumable
+    print("You come across an abandoned campsite, this looks to be a safe place to rest and recover.")
+    input("Press any key to continue")
+    player_stats["cur_hp"] = player_stats["max_hp"]
+    player_stats["cur_mana"] = player_stats["max_mana"]
 
 def room_transition():
     pass
