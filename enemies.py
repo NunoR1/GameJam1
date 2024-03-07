b6f1d9486ce6ee70be78b071bdccc87ba2c06913
lgift <lpgift@my.waketech.edu>
2024-03-07 13:06:45 -0500

    enemy name values
    enemy nam vlaue

--- enemies.py
+++ enemies.py
@@ -1,9 +1,7 @@
 # common enemies
-enemies = {"Bear": {"health": 20, "damage": 5, "gold": 10},
-           "Wolf": {"health": 20, "damage": 5, "gold": 10},
-           "Troll": {"health": 20, "damage": 5, "gold": 10},
-           "Goblin": {"health": 20, "damage": 5, "gold": 10},
-           "Psychotic Little Fairy": {"health": 20, "damage": 5, "gold": 10}
+enemies = {"Bear": {"name": "Bear", "health": 20, "damage": 5, "gold": 10},
+           "Wolf": {"name": "Wolf", "health": 20, "damage": 5, "gold": 10},
+           "Troll": {"name": "Troll", "health": 20, "damage": 5, "gold": 10},
+           "Goblin": {"name": "Goblin", "health": 20, "damage": 5, "gold": 10},
+           "Psychotic Little Fairy": {"name": "Psychotic Little Fairy", "health": 20, "damage": 5, "gold": 10}
            }
-
-elite_enemies = {}
