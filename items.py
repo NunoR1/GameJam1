f111c4199f3509f329de5449137fa75c32300aaf
pfthomas-1 <145382532+pfthomas-1@users.noreply.github.com>
2024-03-07 12:31:30 -0500

    added nested dictionaries

--- items.py
+++ items.py
@@ -7,21 +7,30 @@ weapon_spear = {"name":"Spear", "attack":7, "gold_value":15, "type":"weapon", "m
 weapon_greatsword = {"name":"Greatsword", "attack":10, "gold_value":30, "type":"weapon", "magic_mod":0.5}
 weapon_magicsword = {"name":"Magic Sword", "attack":7, "gold_value":30, "type":"weapon", "magic_mod":1.75}
 
+weapons = {"Old Sword":weapon_oldsword, "Sword":weapon_goodsword, "Staff":weapon_staff, "Axe":weapon_axe, "Spear":weapon_spear, "Greatsword":weapon_greatsword, "Magic Sword":weapon_magicsword}
+
 # Armor
 armor_leather = {"name":"Leather Armor", "hp_buff": 10, "gold_value":15, "type":"armor"}
 armor_tabbard = {"name":"Tabbard", "hp_buff": 20, "gold_value":25, "type":"armor"}
 armor_mail = {"name":"Mail Armor", "hp_buff": 35, "gold_value":40, "type":"armor"}
+
+armors = {"Leather Armor":armor_leather, "Tabbard":armor_tabbard, "Mail Armor":armor_mail}
 
 # Shields
 shield_plank = {"name":"Plank Shield", "hp_buff": 5, "gold_value":7, "type":"shield"}
 shield_round = {"name":"Round Shield", "hp_buff": 10, "gold_value":15, "type":"shield"}
 shield_tower = {"name":"Tower Shield", "hp_buff": 20, "gold_value":25, "type":"shield"}
 
+shields = {"Plank Shield":shield_plank, "Round Shield":shield_round, "Tower Shield":shield_tower}
+
 # Spellbooks
 spell_book = {"name":"Spellbook", "offense_spell_name":"Blast", "offense_spell_damage":6, "offense_spell_cost":3, "defense_spell_name":"Heal", "defense_spell_healing":4, "defense_spell_cost": 3, "gold_value":30, "type":"spellbook"}
 
 # Consumables
-item_healpotion = {"name":"Healing Potion", "effect":"health_restore", "strength":10, "is_magic": False, "gold_value":8, "type":"consumable_potion"}
-item_manapotion = {"name":"Mana Potion", "effect":"mana_restore", "strength":10, "is_magic": False, "gold_value":8, "type":"consumable_potion"}
-item_lightningWand = {"name":"Wand of Lightnging", "effect":"damage", "strength":10, "is_magic": True, "gold_value":16, "type":"consumable_weapon"}
-item_crossbow = {"name":"Crossbow", "effect":"damage", "strength":12, "is_magic": False, "gold_value":14, "type":"consumable_weapon"}
+
+item_healpotion = {"name":"Healing Potion", "effect":"health_restore", "strength":10, "is_magic": False, "gold_value":8, "type":"consumable"}
+item_manapotion = {"name":"Mana Potion", "effect":"mana_restore", "strength":10, "is_magic": False, "gold_value":8, "type":"consumable"}
+item_lightningWand = {"name":"Wand of Lightnging", "effect":"damage", "strength":10, "is_magic": True, "gold_value":16, "type":"consumable"}
+item_crossbow = {"name":"Crossbow", "effect":"damage", "strength":12, "is_magic": False, "gold_value":14, "type":"consumable"}
+
+consumables = {"Healing Potion":item_healpotion, "Mana Potion":item_manapotion, "Lightning Wand":item_lightningWand, "Crossbow":item_crossbow}
