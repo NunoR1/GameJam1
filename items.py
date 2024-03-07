db96b1fe30353e35b1f9864dc544313e532153de
pfthomas-1 <145382532+pfthomas-1@users.noreply.github.com>
2024-03-07 12:22:24 -0500

    Finished Items

--- items.py
+++ items.py
@@ -1,14 +1,27 @@
 # Weapons
 weapon_oldsword = {"name":"Old Sword", "attack":3, "gold_value":5, "type":"weapon", "magic_mod":1}
+weapon_goodsword = {"name":"Sword", "attack":6, "gold_value":12, "type":"weapon", "magic_mod":1}
+weapon_staff = {"name":"Staff", "attack":3, "gold_value":30, "type":"weapon", "magic_mod":2.5}
+weapon_axe = {"name":"Axe", "attack":8, "gold_value":18, "type":"weapon", "magic_mod":0.8}
+weapon_spear = {"name":"Spear", "attack":7, "gold_value":15, "type":"weapon", "magic_mod":1}
+weapon_greatsword = {"name":"Greatsword", "attack":10, "gold_value":30, "type":"weapon", "magic_mod":0.5}
+weapon_magicsword = {"name":"Magic Sword", "attack":7, "gold_value":30, "type":"weapon", "magic_mod":1.75}
 
 # Armor
 armor_leather = {"name":"Leather Armor", "hp_buff": 10, "gold_value":15, "type":"armor"}
+armor_tabbard = {"name":"Tabbard", "hp_buff": 20, "gold_value":25, "type":"armor"}
+armor_mail = {"name":"Mail Armor", "hp_buff": 35, "gold_value":40, "type":"armor"}
 
 # Shields
+shield_plank = {"name":"Plank Shield", "hp_buff": 5, "gold_value":7, "type":"shield"}
+shield_round = {"name":"Round Shield", "hp_buff": 10, "gold_value":15, "type":"shield"}
+shield_tower = {"name":"Tower Shield", "hp_buff": 20, "gold_value":25, "type":"shield"}
 
 # Spellbooks
-book_mana = {"name":"Tome of Mana", "offense_spell_name":"Blast", "offense_spell_damage":7, "offense_spell_cost":2, "defense_spell_name":"Mend Wounds", "defense_spell_healing":4, "defense_spell_cost": 3, "gold_value":30, "type":"spellbook"}
-book_spirit = {"name":"Book of Spirits", "offense_spell_name":"Invoke Spirits", "offense_spell_damage":5, "offense_spell_cost":2}
-book_faith = {}
+spell_book = {"name":"Spellbook", "offense_spell_name":"Blast", "offense_spell_damage":6, "offense_spell_cost":3, "defense_spell_name":"Heal", "defense_spell_healing":4, "defense_spell_cost": 3, "gold_value":30, "type":"spellbook"}
 
 # Consumables
+item_healpotion = {"name":"Healing Potion", "effect":"health_restore", "strength":10, "is_magic": False, "gold_value":8, "type":"consumable_potion"}
+item_manapotion = {"name":"Mana Potion", "effect":"mana_restore", "strength":10, "is_magic": False, "gold_value":8, "type":"consumable_potion"}
+item_lightningWand = {"name":"Wand of Lightnging", "effect":"damage", "strength":10, "is_magic": True, "gold_value":16, "type":"consumable_weapon"}
+item_crossbow = {"name":"Crossbow", "effect":"damage", "strength":12, "is_magic": False, "gold_value":14, "type":"consumable_weapon"}
--- paths.py
+++ paths.py
@@ -23,14 +23,32 @@ def floor_item_room():
     pass
     # choose between three items until you have 3 consumables
 
-def trial_room():
-    pass
+def trial_room(player_gold, player_attack, player_health):
     # if the player completes a trial, gain a buff
-    trial_types = ["wealth", "ring", "might", "vitality"]
+    trial_list = ["wealth", "vitality"]
+    trial_type = random.choice(trial_list)
+
     # Trial of Wealth: have a certain amount of gold
-    # Trial of the Ring: have a ring in your inventory
-    # Trial of Might: have a certain amount of attack power
+    if trial_type == "wealth":
+        print("The ghost of a long-dead merchant floats before you \"Traveler,\" he says, \"I offer the trial of wealth, carry 55 gold and recieve my boon.\"")
+        input("Press any key to commence the trial of wealth")
+        if player_gold >= 55:
+            print("The merchant nods, \"You have passed my test, you may receive my boon. The blessing of life\"")
+            print("MAX HP INCREASED")
+            player_hp_bonus += 15
+        else:
+            print("The merchant frowns, \"You do not have enough gold, you cannot receive my boon.\"")
+    
     # Trial of Vitality: have a certain amount of health
+    if trial_type == "vitality":
+        print("The ghost of a slain knight floats before you \"Traveler,\" he says, \"I offer the trial of vitality, possess 50 max health and recieve my boon.\"")
+        input("Press any key to commence the trial of vitality")
+        if player_health >= 50:
+            print("\"You have passed my test, traveler.\" The knight bows his head, \"With the boon of strength, your blow shall strike true. May you succeed where I have failed, slay that monster.\"")
+            print("DAMAGE INCREASED")
+            player_attack_bonus += 5
+        else:
+            print("\"I apologize,\" the knight says, \"you lack the endurance to receive my blessing.\"")
 
 def boss_room():
     pass
@@ -44,3 +62,6 @@ def campfire_room():
 def loot_room():
     pass
     # gain three random items, can be main_hand, off_hand, armor, or consumable
+
+def room_transition():
+    pass
