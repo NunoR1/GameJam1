fbeeefea0aa103fd2400b607aac1c5f97fee1dcf
lgift <lpgift@my.waketech.edu>
2024-03-07 11:47:54 -0500

    enemies 3
    assigns value of gold for the enemies to drop

--- enemies.py
+++ enemies.py
@@ -1,10 +1,9 @@
 # common enemies
-enemies = {"Bear": {"health": 20, "damage": 5},
-           "Wolf": {"health": 20, "damage": 5},
-           "Troll": {"health": 20, "damage": 5},
-           "Goblin": {"health": 20, "damage": 5},
-           "Psychotic Little Fairy": {"health": 20, "damage": 5}
+enemies = {"Bear": {"health": 20, "damage": 5, "gold": 10},
+           "Wolf": {"health": 20, "damage": 5, "gold": 10},
+           "Troll": {"health": 20, "damage": 5, "gold": 10},
+           "Goblin": {"health": 20, "damage": 5, "gold": 10},
+           "Psychotic Little Fairy": {"health": 20, "damage": 5, "gold": 10}
            }
 
 elite_enemies = {}
-
